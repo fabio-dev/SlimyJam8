9ce6723bb1ad1cbdcd67debe2a56515a615a15d3
Fabio Godinho <fabio.godinho92@outlook.com>
2025-07-24 22:26:38 +0200

    Possibilité de spawn des sneks

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -4,7 +4,7 @@ using Assets.Scripts.Domain;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private EnemyGO _enemyPrefab;
+    [SerializeField] private EnemyGO[] _enemyPrefabs;
     [SerializeField] private float _spawnInterval = 2f;
     [SerializeField] private float _spawnRadiusMin = 8f;
     [SerializeField] private float _spawnRadiusMax = 12f;
@@ -37,9 +37,16 @@ public class EnemySpawner : MonoBehaviour
         float randomDistance = Random.Range(_spawnRadiusMin, _spawnRadiusMax);
         Vector2 spawnPosition = (Vector2)_player.transform.position + randomDir * randomDistance;
 
-        EnemyGO enemyGO = Instantiate(_enemyPrefab, spawnPosition, Quaternion.identity);
+        EnemyGO enemyPrefab = RandomEnemy();
+        EnemyGO enemyGO = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
 
         Enemy enemy = new Enemy(3.0f, 1.0f, 5.0f);
         enemyGO.Setup(enemy);
+    }
+
+    private EnemyGO RandomEnemy()
+    {
+        int rng = Random.Range(0, _enemyPrefabs.Length);
+        return _enemyPrefabs[rng];
     }
 }
